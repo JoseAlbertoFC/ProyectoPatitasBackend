0f58ea2870eec9100faf60e4e9e720c17d3caf92
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-11-20 19:45:42 -0300

    Listo para empezar a crear rutas

--- Api/src/app.js
+++ Api/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
+
+app.use(morgan("dev"));
 
 module.exports = app;
-
-// ñkjnsfvjwro9
